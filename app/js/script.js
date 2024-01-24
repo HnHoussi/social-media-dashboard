39365e14717b1e965c50f1747ec0d29c96300582
Houcine Hannoun <hanoun.houssin@gmail.com>
2024-01-24 12:20:22 +0100

    refactor the code and add favicon

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,3 @@
-
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 
@@ -19,16 +18,14 @@ const colorModeFromLocalStorage = () => {
 const colorModeFromPreferences = () => {
   return window.matchMedia('(prefers-color-scheme: dark)').matches 
               ? 'dark'
-              : 'light' // If preference is set or does not match anything (light is default)
+              : 'light';
 };
 
 const loadAndUpdateColor = () => {
-  // local storage has precendence over the prefers-color-scheme
   const color = colorModeFromLocalStorage() || colorModeFromPreferences();
   color == 'dark' ? darkButton.click() : lightButton.click();
 };
 
-// when the inputs are clicked, check which radio button is checked and change the color
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
 radioButtons.forEach(button => {
   button.addEventListener('click', (event) => {
@@ -36,12 +33,12 @@ radioButtons.forEach(button => {
   });
 });
 
-// when the prefers-color-scheme changes, this event will be emitted
-// event reflects the media query, if it matches, the new color is dark, else it is light
+
 window.matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', (event) => {
         event.matches ? darkButton.click() : lightButton.click();
       });
       
-// Load the right color on startup - localStorage has precedence
+
+
 loadAndUpdateColor();
